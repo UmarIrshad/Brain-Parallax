961fbb808e4e7b8bdf47b9bd9da41031412e1fa4
Umar Irshad <umarirshad@me.com>
2026-06-18 23:04:22 +0500

    Removed version 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import { useState } from "react";
 import ParallaxSection from "./components/ParallaxSection";
-import Version2 from "./components/Version2";
-import VersionSwitcher, { type Version } from "./components/VersionSwitcher";
 
 export default function App() {
-  const [version, setVersion] = useState<Version>(1);
-
   return (
     <main>
-      <VersionSwitcher active={version} onChange={setVersion} />
-      {version === 1 ? <ParallaxSection /> : <Version2 />}
+      <ParallaxSection />
     </main>
   );
 }
